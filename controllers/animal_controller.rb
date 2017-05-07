1622c3a397b7b0fba02b985d8a51d91c824856f4
Louise Camlin <lacamlin@gmail.com>
2017-05-07 17:23:54 +0100

    add other erb files

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -12,13 +12,31 @@ get '/animals/new' do
   erb(:"animals/new")
 end
 
+post "/animals" do
+  @animal = Animal.new(params)
+  @animal.save
+  erb(:"animals/create")
+end
+
 get "/animals/:id" do 
   @animal = Animal.find(params[:id])
   erb(:"animals/show")
 end
 
-post "/animals" do
+
+get "animals/:id/edit" do
+  @animal = Animal.find(params[:id])
+  erb(:"animals/edit")
+end
+
+post '/animals/:id' do
   @animal = Animal.new(params)
-  @animal.save
-  erb(:"animals/create")
+  @animal.update()
+  erb(:update)
+end
+
+delete '/animals/:id' do
+  @animal = Animal.find(params[:id])
+  @animal.delete
+  erb(:destroy)
 end
